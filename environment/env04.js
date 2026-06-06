907a20841e34803a852fc5ea43f7fc783aace772
mirandapalladino <mirandapalladino@gmail.com>
2026-06-05 23:14:51 -0700

    lab 4 !

---
+++ environment/env04.js
@@ -0,0 +1,17 @@
+let environmentTitle = "Redrocket's house!!";
+
+let environmentElements = ["pet alien", "fuel coffee kettle", "planetarium", "house plant (for oxygen)", "alien toys", "black hole carpet"];
+
+let html = ""
+let index = 0;
+
+$("#show-more").click(function() {
+    if(index <= 5) {
+        html = html + "<li>" + environmentElements[index] + "</li>"
+        index = index + 1;
+        $("#appliance-list").html(html)
+    } else {
+        $("#show-more").html("thats it!")
+    }
+})
+
